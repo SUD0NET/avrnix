d483807109301ee24ce30581a4b7bbf7f2d238aa
Adnan Saad <the.lone.comrade.was.here@gmail.com>
2025-09-24 21:46:47 +1000

    Create avrnix.h

---
+++ include/avrnix.h
@@ -0,0 +1,30 @@
+/* avrnix.h - include all system header files */
+
+/* avr specific */
+#include <avr_messages.h>
+
+#include <kernel.h>
+#include <conf.h>
+#include <process.h>
+#include <queue.h>
+#include <resched.h>
+#include <semaphore.h>
+#include <memory.h>
+#include <bufpool.h>
+#include <mark.h>
+#include <ports.h>
+#include <timer.h>
+#include <uart.h>	/* avr UART peripheral */
+#include <tty.h>
+#include <gpio.h>	/* avr GPIO peripherals */
+#include <gpioselect.h>
+#include <device.h>
+#include <interrupt.h>
+#include <spi.h>
+#include <name.h>
+#include <date.h>
+#include <prototypes.h>
+#include <delay.h>
+#include <stdio.h>
+#include <string.h>
+#include <syscall.h>
