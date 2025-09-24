6faf48db6bc90db8bebf2ddf963882ae8cce0c0e
Adnan Saad <the.lone.comrade.was.here@gmail.com>
2025-09-24 21:48:41 +1000

    all include files

---
+++ include/shprototypes.h
@@ -0,0 +1,91 @@
+/* in file sh_argecho.c */
+extern	shellcmd  sh_argecho	(int32, char *[]);
+
+/* in file sh_arp.c */
+extern	shellcmd  sh_arp	(int32, char *[]);
+
+/* in file sh_bingid.c */
+extern	shellcmd  sh_bingid	(int32, char *[]);
+
+/* in file sh_cat.c */
+extern	shellcmd  sh_cat	(int32, char *[]);
+
+/* in file sh_clear.c */
+extern	shellcmd  sh_clear	(int32, char *[]);
+
+/* in file sh_date.c */
+extern	shellcmd  sh_date	(int32, char *[]);
+
+/* in file sh_devdump.c */
+extern	shellcmd  sh_devdump	(int32, char *[]);
+
+/* in file sh_echo.c */
+extern	shellcmd  sh_echo	(int32, char *[]);
+
+/* in file sh_ethstat.c */
+extern	shellcmd  sh_ethstat	(int32, char *[]);
+
+/* in file sh_exit.c */
+extern	shellcmd  sh_exit	(int32, char *[]);
+
+/* in file sh_help.c */
+// extern	shellcmd  sh_help	(int32, char *[]);
+
+/* in file sh_kill.c */
+// extern	shellcmd  sh_kill	(int32, char *[]);
+
+/* in file sh_ls.c */
+extern	shellcmd  sh_ls	(int32,	char *[]);
+
+/* in file sh_led.c */
+extern	shellcmd  sh_led	(int32, char *[]);
+
+/* in file sh_memdump.c */
+extern	shellcmd  sh_memdump	(int32, char *[]);
+
+/* in file sh_memdump.c */
+extern	shellcmd  sh_memdump	(int32, char *[]);
+
+/* in file sh_netinfo.c */
+extern	shellcmd  sh_netinfo	(int32, char *[]);
+
+/* in file sh_nvram.c */
+extern	shellcmd  sh_nvram	(int32, char *[]);
+
+/* in file sh_ping.c */
+extern	shellcmd  sh_ping	(int32, char *[]);
+
+/* in file sh_ps.c */
+extern	shellcmd  sh_ps	(int32, char *[]);
+
+/* in file sh_sleep.c */
+extern	shellcmd  sh_sleep	(int32, char *[]);
+
+/* in file sh_udpdump.c */
+extern	shellcmd  sh_udpdump	(int32, char *[]);
+
+/* in file sh_udpecho.c */
+extern	shellcmd  sh_udpecho	(int32, char *[]);
+
+/* in file sh_udpeserver.c */
+extern	shellcmd  sh_udpeserver (int32, char *[]);
+
+/* in file sh_uptime.c */
+extern	shellcmd  sh_uptime	(int32, char *[]);
+
+/* in file sh_help.c */
+// extern	shellcmd  sh_help	(int32, char *[]);
+
+
+// RAFA AGREGO TODO ESTO
+/* in file sh_editor.c */
+extern	shellcmd  sh_editor	(int32, char *[]);
+extern	shellcmd  sh_basic	(int32, char *[]);
+extern	shellcmd  sh_ps	(int32, char *[]);
+extern	shellcmd  sh_free	(int32, char *[]);
+extern	shellcmd  sh_forever	(int32, char *[]);
+extern	shellcmd  sh_reboot	(int32, char *[]);
+extern	shellcmd  sh_cal	(int32, char *[]);
+extern	shellcmd  sh_date	(int32, char *[]);
+extern	shellcmd  sh_gpio	(int32, char *[]);
+extern	shellcmd  sh_bell	(int32, char *[]);
